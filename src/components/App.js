93925d9007326b914bc9442e83b994caba8ded61
Qajay0832 <qajay0832@gmail.com>
2024-10-04 07:09:36 +0530

    second

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ const App = () => {
         {/* Do not remove the main div */}
         <h1>Sum Calculator</h1>
         <input type='number' value={input} onChange={(e)=>setInput(e.target.value)}></input>
-        <p>Sum:{sum}</p>
+        <p>{'Sum: '+sum}</p>
     </div>
   )
 }
